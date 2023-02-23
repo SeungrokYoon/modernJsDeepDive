02c0d076c26950be7e6c9c57d12c54734027cf98
Seungrok <sonicrok93@gmail.com>
2023-02-23 22:36:25 +0900

    fix: fix eslint error
    the cause of the error was ESLint server did not reflected new configs. Restarting ESLint server fixed the error

--- chapters/chapter9.js
+++ chapters/chapter9.js
@@ -3,3 +3,7 @@ const a = Boolean('x')
 const sum = (a, b) => a + b
 
 module.exports = sum
+
+//falsy 값인 0, 빈 문자열 ''도 기본값으로서 유효하다면 예기치 않은 동작이 발생할 수 있다.
+const foo = '' || 'default String'
+console.log(foo)
