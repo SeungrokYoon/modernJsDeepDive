9bc24a7f7c33dc4f31e33ca18d2422cfdc98b1d0
Seungrok <sonicrok93@gmail.com>
2023-02-26 21:50:48 +0900

    feat: added Chapter12-Function part 1

---
+++ chapters/chapter12-part1.js
@@ -0,0 +1,115 @@
+/**
+ * Theme: Function
+ */
+
+//paramters are x,y
+function add(x, y) {
+  return x + y
+}
+
+//We need to pass arguments(actual values) to a functino to operate it
+add(2, 5)
+
+//In JS, funtions are Object data type. Therefore, we can name it
+const variable = function add(x, y) {
+  return x + y
+}
+
+/**
+ * Functions can be optionally named.
+ *  Named one is called 'named function',
+ *  and unnamed one is called 'anonymous function'.
+ *
+ * Functions are evaluated and have values. That value is object type.
+ * Therefore, functions are objects.
+ *
+ * Funtion objects are different from normal objects.
+ *  Functions are callable, whereas normal objects are not.
+ * Function objects have its discriminated properties that normal objects don't have.
+ *
+ * There are 4 ways of defining functions.
+ *
+ * In JS, defining is automatiaclly followed by declarating functions.
+ * Defining is  allocating memory to declared one, creating identifiers.
+ *
+ */
+
+/**
+ * Declaration
+ * function declaration looks the same as functino literal. However, declaration cannot omit name.
+ * Declaration is not a statement which is not an expression(of course, there exists a statement which is an expression vice versa)
+ */
+
+function subtract(x, y) {
+  return Math.max(x, y) - Math.min(x, y)
+}
+
+// Expression
+const subtract2 = function (x, y) {
+  return Math.max(x, y) - Math.min(x, y)
+}
+
+// Function constructor
+const multiply = new Function('x', 'y', 'return x*y')
+
+// arrow function
+const arrow = (x, y) => x + y
+
+function foo() {
+  console.log('foo')
+}
+
+foo()
+
+const a = function bar() {
+  console.log('bar')
+}
+
+//bar is not defined <- why? named function literal is interpreted differently by context.
+bar()
+
+// function name is valid only inside the function
+const add3 = function foo3(x, y) {
+  //foo3(x+1, y+1) possible since function name is valid only inside the function
+  return x + y
+}
+//valid
+add3(2, 5)
+//foo3 is not defined
+foo3()
+
+/**
+ * Function definition by declaration and function expression are different
+ *
+ * in that functions defined by declaration can be called before its declaration on the
+ * other hand functions defined by expression cannot be called before its expression.
+ *
+ */
+
+console.dir(declaredFunction)
+console.dir(expressedFunction)
+
+console.log(declaredFunction(2, 5))
+console.log(expressedFunction(2, 5))
+
+function declaredFunction(x, y) {
+  return x + y
+}
+
+const expressedFunction = function (x, y) {
+  return x + y
+}
+
+/**
+ * This difference implies that these two functions are generated in different timeline.
+ *
+ * Declarated function  statements are executed before Javascript runtime. Before runtime, all the declarations are executed.
+ * Expressed function statements are executed at runtime. Therefore, before reading the expressed statements, it cannot be executed.
+ * This kind of  operational uniqueness is called  'function hoisting', which seems like declaration statements are hoisted at the top of the code.
+ * Physically it is not what it looks like...but it looks like :) *
+ *
+ * In declared functions, function hoisting occurs, but value hoising (initializing as undefined) occurs to function expressions
+ *
+ * Douglas Crockford, who invented JSON, encourages to use function expression to avoid hoisting issue.
+ *
+ */
